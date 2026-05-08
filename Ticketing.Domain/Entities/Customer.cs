9aa0e47b5caa80075f87b03962fa3488b626af10
admin <admin@NBCORAR2433>
2026-05-08 11:40:22 -0300

    HomeWork2-sergioCorrillo

--- Ticketing.Domain/Entities/Customer.cs
+++ Ticketing.Domain/Entities/Customer.cs
@@ -1,12 +1,19 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Ticketing.Domain.Entities
 {
     public class Customer
     {
+        [Key]
         public Guid CustomerId { get; set; }
+
+        [Required, EmailAddress, MaxLength(200)]
         public string Email { get; set; } = null!;
+
+        [Required, MaxLength(200)]
         public string FullName { get; set; } = null!;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
--- Ticketing.Domain/Entities/Offer.cs
+++ Ticketing.Domain/Entities/Offer.cs
@@ -1,18 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ticketing.Domain.Entities
 {
     public class Offer
     {
+        [Key]
         public Guid OfferId { get; set; }
+
         public Guid EventId { get; set; }
         public Guid PriceId { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; } = null!;
+
         public bool IsActive { get; set; } = true;
 
+        [ForeignKey(nameof(EventId))]
         public Event? Event { get; set; }
+
+        [ForeignKey(nameof(PriceId))]
         public Price? Price { get; set; }
+
         public ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
     }
 }
